5d02c0b15440eec67a56156b08db6556169a416e
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-14 11:47:14 +0300

    Fixed DRY

--- MNews/Service/NewsService.cs
+++ MNews/Service/NewsService.cs
@@ -3,31 +3,25 @@ using Newtonsoft.Json;
 
 namespace MNews.Service
 {
-    public class NewsService
+    public class NewsService(string requestUrl)
     {
         public async Task<List<ArticleModel>?> GetNewsAsync(string apiKey)
         {
-            using (var httpClient = new HttpClient())
-            {
-                string apiUrl = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=";
-                string requestUrl = $"{apiUrl}{apiKey}";
+            using var httpClient = new HttpClient();
 
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "MN News/1.0");
 
-                HttpClient client = new HttpClient();
-                client.DefaultRequestHeaders.Add("User-Agent", "MN News/1.0");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+            request.Headers.Add("Authorization", "Bearer " + apiKey);
 
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
-                request.Headers.Add("Authorization", "Bearer " + apiKey);
+            HttpResponseMessage CheckStatusCode = await client.SendAsync(request);
 
-                HttpResponseMessage CheckStatusCode = await client.SendAsync(request);
+            using var response = await httpClient.GetAsync(requestUrl);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            var newsApiResponse = JsonConvert.DeserializeObject<ApiResponseModel>(apiResponse);
 
-
-                using var response = await httpClient.GetAsync(requestUrl);
-                string apiResponse = await response.Content.ReadAsStringAsync();
-                var newsApiResponse = JsonConvert.DeserializeObject<ApiResponseModel>(apiResponse);
-
-                return newsApiResponse == null ? throw new Exception("Failed to fetch data from News API.") : newsApiResponse.Articles;
-            }
+            return newsApiResponse == null ? throw new Exception("Failed to fetch data from News API.") : newsApiResponse.Articles;
         }
 
     }
