f37b453b617a1ae83da09d62671d215e7d098a77
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-19 01:25:59 +0300

    A new service class that get and set cache news articles.

---
+++ MNews/Service/SessionHelper.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace MNews.Service
+{
+    public static class SessionHelper
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T? Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+
+}
