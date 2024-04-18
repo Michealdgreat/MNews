05cb81c0639b648741f504ea9b3086cacc96178b
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-19 01:25:13 +0300

    Updated URL, Added new ArticleDetailsLayer: You can now access news content more quickly and directly through MNews. The main URL no longer redirects to the original article; instead, it takes you to an articleDetails page where you can read the news content.

--- MNews/Controllers/HomeController.cs
+++ MNews/Controllers/HomeController.cs
@@ -1,35 +1,38 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MNews.Models;
 using MNews.Service;
 using System.Configuration;
 using System.Diagnostics;
+using System.Security.Policy;
 
-namespace MNews.Controllers
+namespace MNews.Controllers;
+
+public class HomeController(ILogger<HomeController> logger, IConfiguration configuration) : Controller
 {
-    public class HomeController(ILogger<HomeController> logger, IConfiguration configuration) : Controller
+    private readonly ILogger<HomeController> _logger = logger;
+    private readonly string _apiKey = configuration.GetValue<string>("apiKey");
+    List<ArticleModel> news;
+    public async Task<IActionResult> Index()
     {
-        private readonly ILogger<HomeController> _logger = logger;
-        private readonly string _apiKey = configuration.GetValue<string>("apiKey");
+        string apiUrl = "https://newsapi.org/v2/everything?q=finland&apikey=";
+        string GetrequestUrl = $"{apiUrl}{_apiKey}";
 
-        public async Task<IActionResult> Index()
-        {
-            string apiUrl = "https://newsapi.org/v2/everything?q=finland&apikey=";
-            string GetrequestUrl = $"{apiUrl}{_apiKey}";
+        var newsService = new NewsService(GetrequestUrl);
+        news = await newsService.GetNewsAsync(_apiKey);
+        HttpContext.Session.Set("Articles", news);
 
-            var newsService = new NewsService(GetrequestUrl);
-            var news = await newsService.GetNewsAsync(_apiKey);
-            return View(news);
-        }
+        return View(news);
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    public IActionResult ArticleDetails(string url)
+    {
+        // Retrieve the list from session
+        var articles = HttpContext.Session.Get<List<ArticleModel>>("Articles");
+        // Find the specific article
+        var article = articles?.FirstOrDefault(a => a.Title == url);
+
+        return View(article);
     }
 }
--- MNews/Controllers/SearchNewsController.cs
+++ MNews/Controllers/SearchNewsController.cs
@@ -1,22 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MNews.Models;
 using MNews.Service;
 
-namespace MNews.Controllers
+namespace MNews.Controllers;
+
+public class SearchNewsController(IConfiguration configuration) : Controller
 {
-    public class SearchNewsController(IConfiguration configuration) : Controller
+    private readonly string _apiKey = configuration.GetValue<string>("apiKey");
+
+
+    public async Task<IActionResult> SearchByQuery(string SearchQuery)
     {
-        private readonly string _apiKey = configuration.GetValue<string>("apiKey");
+        string apiUrl = "https://newsapi.org/v2/everything?q=";
 
+        string getRequestUrl = $"{apiUrl}{SearchQuery}&apikey={_apiKey}";
 
-        public async Task<IActionResult> SearchByQuery(string SearchQuery)
-        {
-            string apiUrl = "https://newsapi.org/v2/everything?q=";
+        var newsService = new NewsService(getRequestUrl);
+        var news = await newsService.GetNewsAsync(_apiKey);
+        HttpContext.Session.Set("Articles", news);
 
-            string getRequestUrl = $"{apiUrl}{SearchQuery}&apikey={_apiKey}";
+        return View(news);
+    }
 
-            var newsService = new NewsService(getRequestUrl);
-            var news = await newsService.GetNewsAsync(_apiKey);
-            return View(news);
-        }
+    public IActionResult ArticleDetails(string url)
+    {
+        // Retrieve the list from session
+        var articles = HttpContext.Session.Get<List<ArticleModel>>("Articles");
+        // Find the specific article
+        var article = articles?.FirstOrDefault(a => a.Title == url);
+
+        return View(article);
     }
 }
