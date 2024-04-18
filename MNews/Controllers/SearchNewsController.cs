b7f382188b454940da8975763ec8df01b8d58423
Micheal Shodamola <michealshodamola01@gmail.com>
2024-04-18 11:48:59 +0300

    New search Controller that returns articles by query.

---
+++ MNews/Controllers/SearchNewsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MNews.Service;
+
+namespace MNews.Controllers
+{
+    public class SearchNewsController(IConfiguration configuration) : Controller
+    {
+        private readonly string _apiKey = configuration.GetValue<string>("apiKey");
+
+
+        public async Task<IActionResult> SearchByQuery(string SearchQuery)
+        {
+            string apiUrl = "https://newsapi.org/v2/everything?q=";
+
+            string getRequestUrl = $"{apiUrl}{SearchQuery}&apikey={_apiKey}";
+
+            var newsService = new NewsService(getRequestUrl);
+            var news = await newsService.GetNewsAsync(_apiKey);
+            return View(news);
+        }
+    }
+}
